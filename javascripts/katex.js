493a91e3240fdb8df8396599747212121877738d
Awelson <aaronwelson@gmail.com>
2025-12-07 20:51:57 +0700

    Deployed 4b4e196 with MkDocs version: 1.6.1

---
+++ javascripts/katex.js
@@ -0,0 +1,21 @@
+document$.subscribe(({ body }) => { 
+  renderMathInElement(body, {
+    delimiters: [
+      { left: "$$",  right: "$$",  display: true },
+      { left: "$",   right: "$",   display: false },
+      { left: "\\(", right: "\\)", display: false },
+      { left: "\\[", right: "\\]", display: true }
+    ],
+  })
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+  renderMathInElement(document.body, {
+    delimiters: [
+      { left: "$$", right: "$$", display: true },
+      { left: "$", right: "$", display: false }
+    ],
+    throwOnError: false,
+    ignoredTags: []  // <- allows KaTeX inside <code>, <pre>, etc.
+  });
+});
